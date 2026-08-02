b9ea59e7c3b4fb9b854dffbc481bc0ad68e80577
anushmajk <anushmajk@gmail.com>
2026-08-02 22:20:45 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
-<<<<<<< HEAD
 import Home from './pages/Home'
-=======
->>>>>>> d2b248f9e2a0bf78242d83ee102c8cac774bdcbb
 
 const App = () => {
   return (
     <div>
         <Header/>
-<<<<<<< HEAD
         <Home/>
-=======
->>>>>>> d2b248f9e2a0bf78242d83ee102c8cac774bdcbb
     </div>
   )
 }
